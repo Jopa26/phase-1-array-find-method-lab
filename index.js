bea0cebc0e80310c8ecbb177f2c49b492538780d
Luis <jqluis2011@gmail.com>
2022-03-31 18:54:28 -0700

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    let result = record.find(function(winner) {
+      return winner.result === "W"
+    })
+    if (!!result) {
+      return result.year
+    } else {
+      return undefined
+    }
+  }
+
+    
